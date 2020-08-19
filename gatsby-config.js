b18a5c092d75d900d333ed184c517d3816abba75
Alex Benko <benkoalex521@gmail.com>
2020-08-18 23:15:54 -0400

    Learning How Gatsby and GraphQL works first

---
+++ gatsby-config.js
@@ -0,0 +1,43 @@
+const CONTENTFUL = require('./config.js');
+
+module.exports = {
+  siteMetadata: {
+    title: `Webstacks Jr Front End Dev Take Home`,
+    description: 'Figuring out how to use GraphQL to get data from here',
+    author: 'Alexander Benko'
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: CONTENTFUL.spaceId,
+        accessToken: CONTENTFUL.access,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
