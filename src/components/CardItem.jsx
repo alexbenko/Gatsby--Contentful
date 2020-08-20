790656cc6cd5b91f5323c88a53ee7884c5edaec7
Alex Benko <benkoalex521@gmail.com>
2020-08-20 11:21:55 -0400

    Imported the css file and better inline CSS

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './card.css'
 
 const CardItem = ({data})=>{
     return(
-        <div className="cardItem" style={{backgroundColor:'white',textAlign:'center',width:'33%',}}>
+        <div className="cardItem" style={{backgroundColor:'white',textAlign:'center',width:'33%',margin:'1%'}}>
           {console.log(data.node)}
           <img style={{height:'25%'}} src={data.node.icon.file.url} alt="Displays something relevant "></img>
           <h1 style={{textDecoration:'none'}}>{data.node.header}</h1>
