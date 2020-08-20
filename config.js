b3d7937386fc69e7d9b031e4c5fe6cb6a20bcb62
Alex Benko <benkoalex521@gmail.com>
2020-08-20 10:19:37 -0400

    (init)

---
+++ config.js
@@ -0,0 +1,7 @@
+//I would normally ignore this file, but did not just in case you clone it down
+module.exports = {
+  management:'CFPAT-2T13arMQrWUQY05LIuv2Jpwv1E6vprpHfdKpU_7Crr4',
+  access:'5QqdZVdaauvAdHv712IsOUhM0q7a4Si8XYeXgEzj_dQ',
+  preview:'C32Nj2PW8aPV7Ai2cwtk0c6YD9KAwN_Or4iuvMaLpgc',
+  spaceId:'byrtytwog8g4'
+};
