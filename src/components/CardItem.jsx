e21a8ab888a66b54317bfa60733f100d2b4a8a2c
Alex Benko <benkoalex521@gmail.com>
2020-08-19 22:06:22 -0400

    Looks more closely to the mockup

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
 
-class CardItem extends Component{
-  constructor(props){
-    super(props);
-  }
-
-  render(){
+const CardItem = ({data})=>{
     return(
-      <div className="cardItem">
-
-      </div>
+        <div className="cardItem" style={{backgroundColor:'white',textAlign:'center',margin:'0 -15px'}}>
+          <h1>{data.node.header}</h1>
+          <img style={{mixBlendMode: 'multiply'}} src={data.node.icon.file.url} alt="Displays something relevant "></img>
+          <p>{data.node.description.description}</p>
+        </div>
     )
-  }
 }
+
+export default CardItem;
