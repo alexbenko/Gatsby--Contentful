07c46e506a51d3067efe0d0e92ea3a295b1bb895
Alex Benko <benkoalex521@gmail.com>
2020-08-19 22:01:27 -0400

    Renders Card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,14 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from '../components/Hero'
+import Cards from '../components/Cards.jsx';
 
 const IndexPage = () => (
-  <div style={{backgroundColor:'#A9A9A9'}}>
     <Layout>
       <SEO title="Front End Jr Dev Take Home" />
       <Hero />
+      <Cards />
     </Layout>
-  </div>
 )
 
 export default IndexPage
