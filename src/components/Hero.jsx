f40a2eefee8803d7c64fd0f5a3347ef0ee886b68
Alex Benko <benkoalex521@gmail.com>
2020-08-19 22:03:04 -0400

    Fixed so the grey is only in the Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,9 +37,8 @@ const Hero = () =>{
   let subHeader = data.allContentfulHero.nodes[0].subheader;
   let link = data.allContentfulHero.nodes[0].url;
   return(
-    <div style={{padding: '15%'}}>
+    <div style={{padding: '15%',backgroundColor:'#A9A9A9'}}>
       <div className="Hero-Holder" style={{textAlign:'center',color:'white',fontSize:'1em'}}>
-        {console.log(data.allContentfulHero.nodes[0])}
         <h1 style={style.header}>{header}</h1>
         <h3 style={{wordWrap: 'normal',fontSize:'1em'}}>{subHeader}</h3>
         <a href={link} target="_blank" rel="noopener noreferrer"><button style={style.button}>Over To You</button></a>
